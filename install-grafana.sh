a978ecd5a6a798f15d3e8177f2777e27aefe893d
rizqiadnan <48936419+rizqiadnan@users.noreply.github.com>
2022-09-07 11:29:39 +0700

    first init :)

---
+++ install-grafana.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y adduser libfontconfig1
+wget https://dl.grafana.com/oss/release/grafana_9.1.2_amd64.deb
+sudo dpkg -i grafana_9.1.2_amd64.deb
+sudo systemctl daemon-reload
+sudo systemctl start grafana-server
+sudo systemctl status grafana-server
+sudo systemctl enable grafana-server.service
